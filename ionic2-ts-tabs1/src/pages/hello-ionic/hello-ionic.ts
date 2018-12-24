0cc888f693b725bcf99b4c46df368fb2ae4a1dff
webgatto <info@sap-en-sky.ru>
2018-12-24 18:41:33 +0300

    Добавьте файлы проекта.

---
+++ ionic2-ts-tabs1/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+
+  constructor() {
+
+  }
+
+}
---
+++ ionic2-ts-tabs1/src/pages/item-details/item-details.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-item-details',
+  templateUrl: 'item-details.html'
+})
+export class ItemDetailsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
