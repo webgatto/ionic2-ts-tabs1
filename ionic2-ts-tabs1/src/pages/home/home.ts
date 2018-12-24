27e1fc505a81192ed3c306485dc26acfa98b0097
webgatto <info@sap-en-sky.ru>
2018-12-24 21:04:56 +0300

    all

--- ionic2-ts-tabs1/src/pages/home/home.ts
+++ ionic2-ts-tabs1/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CallNumber } from '@ionic-native/call-number';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private callNumber: CallNumber) {
 
+    this.callNumber.callNumber("79030125075", true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
   }
 
 }
